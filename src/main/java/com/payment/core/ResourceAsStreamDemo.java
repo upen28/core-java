6697e33505db021a39e711ba8f59d9d86c8bab20
upendra28 <upendra.verma@renovite.com>
2019-09-11 13:34:42 +0530

    resource as stream RND

---
+++ src/main/java/com/payment/core/ResourceAsStreamDemo.java
@@ -0,0 +1,16 @@
+package com.payment.core;
+
+import java.io.InputStream;
+
+//path that start with '/' i relative and that does no start with '/' is absolute
+public class ResourceAsStreamDemo {
+
+	public static void main(String... args) {
+		InputStream stream = ResourceAsStreamDemo.class.getResourceAsStream("/generic-packager.xml");
+		if (stream != null) {
+			System.out.println("resource loaded successfully");
+		} else {
+			System.out.println("resource not loaded successfully");
+		}
+	}
+}
